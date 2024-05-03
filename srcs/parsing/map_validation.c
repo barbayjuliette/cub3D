d985267a53f55c055d041a96eb23041ce2c4a59e
Juliette Barbay <jbarbay@c1r7s7.42singapore.sg>
2024-05-03 14:03:55 +0800

    removed num error msg

--- srcs/parsing/map_validation.c
+++ srcs/parsing/map_validation.c
@@ -26,19 +26,19 @@ int	square_is_empty(int i, int j, t_game_data *data)
 void	check_walls(int i, int j, int total_rows, t_game_data *data)
 {
 	if ((i == 0 || i == total_rows - 1) && data->map[i][j] != '1')
-		error_parsing("Map must be enclosed by walls 1", NULL, NULL, data);
+		error_parsing("Map must be enclosed by walls", NULL, NULL, data);
 	else if (i > 0 && square_is_empty(i - 1, j, data) && data->map[i][j] != '1')
-		error_parsing("Map must be enclosed by walls 11", NULL, NULL, data);
+		error_parsing("Map must be enclosed by walls", NULL, NULL, data);
 	else if (data->map[i + 1] && square_is_empty(i + 1, j, data)
 		&& data->map[i][j] != '1')
-		error_parsing("Map must be enclosed by walls 22", NULL, NULL, data);
+		error_parsing("Map must be enclosed by walls", NULL, NULL, data);
 	else if (j == 0 && data->map[i][j] != '1')
-		error_parsing("Map must be enclosed by walls 2", NULL, NULL, data);
+		error_parsing("Map must be enclosed by walls", NULL, NULL, data);
 	else if (data->map[i][j + 1] && data->map[i][j + 1] == '\n'
 		&& data->map[i][j] != '1')
-		error_parsing("Map must be enclosed by walls 3", NULL, NULL, data);
+		error_parsing("Map must be enclosed by walls", NULL, NULL, data);
 	else if (!data->map[i][j + 1] && data->map[i][j] != '1')
-		error_parsing("Map must be enclosed by walls 4", NULL, NULL, data);
+		error_parsing("Map must be enclosed by walls", NULL, NULL, data);
 }
 
 void	validate_map(t_game_data *data, int total_rows, int i)
